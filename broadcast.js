4376012b329a07e370561e8ed762118f5955bb07
saifdz1 <40006650+saifdz1@users.noreply.github.com>
2020-02-15 14:28:27 +0000

    Update broadcast.js

--- broadcast.js
+++ broadcast.js
@@ -35,102 +35,118 @@ Client.user.setGame(`SaifDz`,"http://twitch.tv/SaifDz")
 Client.login(process.env.BOT_TOKEN);
 
 
-Client.on("message", async message => { 
-  if(message.content.startsWith(prefix + "bc")) { 
-    const args = message.content.split(" ").slice(1).join(" "); 
-    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('لا تمتلك الصلاحيات الكافية');
-    if(!args) return message.channel.send('يجب عليك  الرسالة'); 
- 
-      let help = new Discord.RichEmbed() 
-          .setColor("RANDOM") 
-          .setThumbnail(message.author.avatarURL) 
-          .setDescription(`**انوع البرودكاست
+client.on("message", async message => { 
+  var command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  if (!message.channel.guild) return;
+  var args = message.content
+    .split(" ")
+    .slice(1)
+    .join(" ");
+  if (command == "bc") {  // للونلاين و الاوفلاين
+    if (!message.member.hasPermission("ADMINISTRATOR")) {
+      return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
+    }
+    if (!args) {
+      return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
+    }
+    message.channel
+      .send(
+        `**هل أنت متأكد من إرسالك البرودكاست؟nمحتوى البرودكاست: `${args}`**` // `${args}` اعرف ان في خطا هنا لو نقدر تحله حله لو ما نقدر كلمني خاص و لو ما قدرت احذف 
+      )
+      .then(m => {
+        m.react("✅").then(() => m.react("❌"));
 
-          1- للكل بدون امبيد
-          2- للكل بامبيد
-          3- اونلاين بدون امبيد
-          4- للالغاء
-          **`);
-         let typesMSG = await  message.channel.sendEmbed(help)
-         var numbers = ["u0030u20E3", "u0031u20E3", "u0032u20E3", "u0033u20E3", "u0034u20E3", "u0035u20E3", "u0036u20E3", "u0037u20E3", "u0038u20E3", "u0039u20E3"]
+        let yesFilter = (reaction, user) =>
+          reaction.emoji.name == "✅" && user.id == message.author.id;
+        let noFiler = (reaction, user) =>
+          reaction.emoji.name == "❌" && user.id == message.author.id;
 
+        let yes = m.createReactionCollector(yesFilter);
+        let no = m.createReactionCollector(noFiler);
 
-       let r1 = await typesMSG.react(numbers[1]); 
-       let r2 = await typesMSG.react(numbers[2]); 
-       let r3 = await typesMSG.react(numbers[3]); 
-       let r4 = await typesMSG.react("❌"); 
+        yes.on("collect", v => {
+          m.delete();
+          message.channel
+            .send(
+              `:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.memberCount} Members`
+            )
+            .then(msg => msg.delete(5000));
+          message.guild.members.forEach(member => {
+            let bc = new Discord.RichEmbed()
+              .setColor("#0984e3")
+              .setThumbnail(member.user.avatarURL)
+              .setTitle("Broadcast")
+              .addField("Server", message.guild.name)
+              .addField("your name", `<@${member.id}>`)
+              .addField("Massage", args);
+            message.delete();
+            member.sendEmbed(bc);
+          });
+        });
+        no.on("collect", v => {
+          m.delete();
+          message.channel
+            .send("**Broadcast Canceled.**")
+            .then(msg => msg.delete(3000));
+        });
+      });
+  }
+  if (command == "bco") { //للونلاين فقط
+    if (!message.member.hasPermission("ADMINISTRATOR")) {
+      return message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
+    }
+    if (!args) {
+      return message.reply("**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**");
+    }
+    message.channel
+      .send(
+        `**هل أنت متأكد من إرسالك البرودكاست؟nمحتوى البرودكاست: `${args}`**`
+      )
+      .then(m => {
+        m.react("✅").then(() => m.react("❌"));
 
+        let yesFilter = (reaction, user) =>
+          reaction.emoji.name == "✅" && user.id == message.author.id;
+        let noFiler = (reaction, user) =>
+          reaction.emoji.name == "❌" && user.id == message.author.id;
 
+        let yes = m.createReactionCollector(yesFilter);
+        let no = m.createReactionCollector(noFiler);
 
+        yes.on("collect", v => {
+          m.delete();
+          message.channel
+            .send(
+              `:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${
+                message.guild.members.filter(
+                  r => r.presence.status !== "offline"
+                ).size
+              } Members`
+            )
+            .then(msg => msg.delete(5000));
+          message.guild.members
+            .filter(r => r.presence.status !== "offline")
+            .forEach(member => {
+              let bco = new Discord.RichEmbed()
+                .setColor("#0984e3")
+                .setThumbnail(member.user.avatarURL)
+                .setTitle("Broadcast")
+                .addField("Server", message.guild.name)
+                .addField("your name", `<@${member.id}>`)
+                .addField("Massage", args);
+              message.delete();
 
- let filter1 = (reaction, user) => reaction.emoji.name == numbers[1] && user.id == message.author.id;
- let filter2 = (reaction, user) => reaction.emoji.name == numbers[2] && user.id == message.author.id;
- let filter3 = (reaction, user) => reaction.emoji.name == numbers[3] && user.id == message.author.id;
- let filter4 = (reaction, user) => reaction.emoji.name == "❌" && user.id == message.author.id;
- if (!typesMSG) return;
-
- let f1 = typesMSG.createReactionCollector(filter1, { 
-     time: 18000 
- });
- let f2 = typesMSG.createReactionCollector(filter2, { 
-     time: 18000
- }); 
- let f3 = typesMSG.createReactionCollector(filter3, { 
-     time: 18000
- });
- let f4 = typesMSG.createReactionCollector(filter4, { 
-     time: 18000
- });
-
-
-  
-
-f1.on('collect', async r => { 
-  await typesMSG.delete();
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
-    m.send(`${args}n ${m}`);
-    })
-    message.channel.send(`تم الارسال`); 
-  
-}); 
-
-f2.on('collect', async r => {
-await typesMSG.delete();
-message.guild.members.forEach(m => {
-  var bc = new Discord.RichEmbed()
-  .setThumbnail(message.author.avatarURL)
-  .addField('# | الراسل', message.author) 
-  .addField('# | الرسالة ', args)
-  .addField('# | السيرفر', message.guild.name) 
-  .setColor('RANDOM')
-  m.sendMessage(bc)
+              member.sendEmbed(bco);
+            });
+        });
+        no.on("collect", v => {
+          m.delete();
+          message.channel
+            .send("**Broadcast Canceled.**")
+            .then(msg => msg.delete(3000));
+        });
+      });
+  }
 });
-message.channel.send(`تم الارسال`)
-}); 
-
-f3.on('collect', async r => {
-await typesMSG.delete();
-message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
-  m.send(`${args}n ${m}`); 
-  })
-  message.channel.send(``${message.guild.members.filter(m => m.presence.status !== 'online').size}` : عدد الاعضاء المستلمين`); 
-});
- 
-f4.on('collect', async r => {
-await typesMSG.delete();
-message.channel.send('تم الالغاء بنجاح') 
-}); 
-
-
-  } 
-
-}); 
-f4.on('collect', async r => {
-await typesMSG.delete();
-message.channel.send('تم الالغاء بنجاح') //OMAR#6356
-}); //OMAR#6356
-
-
-  } //OMAR#6356
-
-}); //OMAR#6356
+//Alpha codes CopyRight 2020
